f89e8336575511be04327a4c004c319b9a107c80
tinoe0404 <tinoe044@gmail.com>
2025-06-08 21:25:29 +0200

    added 3 views and 3 urls: checkout, cart, and tinoe_store

---
+++ tinoe_store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+	path('', views.tinoe_store, name="tinoe_store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+
+]
--- tinoe_store/views.py
+++ tinoe_store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def tinoe_store(request):
+	context = {}
+	return render(request, 'tinoe_store/tinoe_store.html', context)
+
+def cart(request):
+	context = {}
+	return render(request, 'tinoe_store/cart.html', context)
+
+def checkout(request):
+	context = {}
+	return render(request, 'tinoe_store/checkout.html', context)
