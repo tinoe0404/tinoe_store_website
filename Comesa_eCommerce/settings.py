bf2496c3b8325676bbc60cbb5d12fe92f2ea17d5
tinoe0404 <tinoe044@gmail.com>
2025-06-20 13:28:51 +0200

    feat(products): Add image upload and rendering support

--- Comesa_eCommerce/settings.py
+++ Comesa_eCommerce/settings.py
@@ -123,6 +123,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+MEDIA_URL = '/images/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
