ab9dfe1331d861bda2b6289a02a1aecedb83fcd4
tinoe0404 <tinoe044@gmail.com>
2025-06-23 22:16:23 +0200

    feat: add checkout page data handling

--- tinoe_store/views.py
+++ tinoe_store/views.py
@@ -1,24 +1,36 @@
 from django.shortcuts import render
 from .models import * 
 
+
 def tinoe_store(request):
-	products = Product.objects.all()
-	context = {'products':products}
-	return render(request, 'tinoe_store/tinoe_store.html', context)
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'tinoe_store/tinoe_store.html', context)
+
 
 def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+       
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0 }
 
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-	else:
-		#Create empty cart for now for non-logged in user
-		items = []
+    context = {'items': items, 'order': order}
+    return render(request, 'tinoe_store/cart.html', context)
 
-	context = {'items':items, 'order':order}
-	return render(request, 'tinoe_store/cart.html', context)
 
 def checkout(request):
-	context = {}
-	return render(request, 'tinoe_store/checkout.html', context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+       
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0 }
+
+    context = {'items': items, 'order': order}
+    return render(request, 'tinoe_store/checkout.html', context)
